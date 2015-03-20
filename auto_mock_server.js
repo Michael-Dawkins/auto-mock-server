3c862d160c1176688d7d69d7720c665f8ae4c43b
Michael Dawkins <michaeldawfr@gmail.com>
2015-03-20 09:30:34 +0100

    [Fix] Support PUT in CORS

--- auto_mock_server.js
+++ auto_mock_server.js
@@ -23,6 +23,7 @@ function init(){
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "X-Requested-With");
 		res.header('Access-Control-Allow-Headers', 'Content-Type');
+		res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
 		next();
 	});
 
